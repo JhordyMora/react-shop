7b41d225a66e2a41470c164072c5008b1e88afc7
JhordyMora <jhordy.mora@web.de>
2023-01-07 13:02:30 +0100

    package.json corrections

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-        templateContent: "./public/index.html",
-        filename: 'index.html',
+        template: "./public/index.html",
+        filename: './index.html',
         })
     ]
 };
