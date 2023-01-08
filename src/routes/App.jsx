2d181bf31a877026f22d35edfc31f59cc218a567
JhordyMora <jhordy.mora@web.de>
2023-01-08 15:36:10 +0100

    routes folder created

---
+++ src/routes/App.jsx
@@ -0,0 +1,35 @@
+// ocupen el comando “rafce” para crear el componente.
+import React from 'react';
+import { BrowserRouter, Routes, Switch, Route } from 'react-router-dom';
+import Layout from '../Containers/Layout';
+import Login from '../Containers/Login';
+import RecoveryPassword from '../Containers/RecoveryPassword'
+import "../styles/global.css";
+
+const App = () => {
+    return (
+        // Manera de hacer en versiones antiguas
+        // <BrowserRouter>
+        // <Switch> en vez de Routes
+        //     <Layout>
+        //         <Route exact path='/' component={Home}/> se usaba component en vez de element
+        //         <Route exact path='/Login' component={Login}/>
+        //         <Route exact path='recovery-password' component={RecoveryPassword}/>
+        //         <Route component={Not Found}/>
+        //     </Layout>
+        // </Switch> 
+        // </BrowserRouter>
+        <BrowserRouter>
+        <Layout>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/login" element={<Login />} />			
+                <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Layout>
+        </BrowserRouter>   
+    )
+}
+
+export default App
