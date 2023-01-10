e7fc564107b7932f78e61ccd4f2d82177fe23d1e
JhordyMora <jhordy.mora@web.de>
2023-01-10 13:20:42 +0100

    added aliases for the icons and logos

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
     mode: "development",
     resolve:{
         extensions: [".js",".jsx"],
+        alias: {
+            "@components": path.resolve(__dirname, "src/components"),
+            "@containers": path.resolve(__dirname, "src/Containers"),
+            "@styles": path.resolve(__dirname, "src/styles"),
+            "@icons": path.resolve(__dirname, "./src/assets/icons"),
+            "@logos": path.resolve(__dirname, "./src/assets/logos"),
+        }
     },
     module: {
         rules: [
