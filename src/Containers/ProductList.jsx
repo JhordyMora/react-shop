c04d455737095d0ed1eaf5662d6fecfa118f9a35
JhordyMora <jhordy.mora@web.de>
2023-01-12 01:03:08 +0100

    connected Product list with api

--- src/Containers/ProductList.jsx
+++ src/Containers/ProductList.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect , useState} from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from "axios";
+
+const API = "https://api.escuelajs.co/api/v1/products"; 
 
 const ProductList = () => {
+	const [products, setProducts]= useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+		const response = await axios(API);
+		setProducts(response.data);
+		}
+		fetchData();
+	}, []);
+
+	// Esta es una manera menos oficial y no recomendada por la documention
+	// useEffect(async ()=>{
+	// 	const response = await axios(API);
+	// 	setProducts(response.data);
+	// }, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
+			{products.map(product =>(
 				<ProductItem />
+			))}
 			</div>
 		</section>
 	);
