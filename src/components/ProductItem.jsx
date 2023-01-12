01fb07cb35b6c4b047c334d6fe039d395834b4ee
JhordyMora <jhordy.mora@web.de>
2023-01-12 21:35:18 +0100

    useRef

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,23 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/ProductItem.scss';
 import addIcon from "@icons/bt_add_to_cart.svg"
 
 const ProductItem = ({product}) => {
-	const [cart, setCart]=useState([]);
-
-	const handleClick = () =>{
-		setCart([]);
-	}
 
 	return (
 		<div className="ProductItem">
 			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>{product.price}</p>
+					<p>{product.price} Euros</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={handleClick}>
+				<figure>
 					<img src={addIcon} alt="" />
 				</figure>
 			</div>
